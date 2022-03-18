28323aae8a96ba417830bec0d63c6b6c83375f4b
Subhodip Kundu <subhodipkundu21@gmail.com>
2022-03-18 19:24:51 +0530

    First Commit of Signup app

---
+++ app.js
@@ -0,0 +1,71 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const https = require("https");
+const { options } = require("request");
+
+const app = express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/", function(req,res){
+    res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function(req,res){
+    const firstName = req.body.fName;
+    const lastName = req.body.lName;
+    const email = req.body.email;
+
+    const data = {
+        members: [
+            {
+                email_address : req.body.email,
+                status : "subscribed",
+                merge_fields: {
+                    FNAME : firstName,
+                    LNAME : lastName
+                }
+            }
+        ]
+    }
+    const jsonData = JSON.stringify(data);
+
+    const url = "https://us14.api.mailchimp.com/3.0/lists/2fac9a0bc1";
+
+    const options = {
+        method : "POST",
+        auth : "subhodip:9338a4c0a625e4e5d01822c2946771d1e-us14"
+    }
+
+    const request = https.request(url, options, function(response){
+        if(response.statusCode === 200){
+            res.sendFile(__dirname + "/success.html");
+        }
+        else{
+            res.sendFile(__dirname + "/failure.html");
+        }
+        response.on("data", function(data){
+            console.log(JSON.parse(data));
+        });
+    });
+
+    request.write(jsonData);
+    request.end();
+});
+
+app.post("/failure", function(req,res){
+    res.redirect("/");
+});
+
+app.listen(process.env.PORT || 3000, function(){
+    console.log("The server has started on port 3000");
+});
+
+
+// API key
+// 938a4c0a625e4e5d01822c2946771d1e-us14
+
+// Audience ID
+// 2fac9a0bc1
